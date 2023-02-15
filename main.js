17408d4d9b1692b24d9a19e2f9ea81a0011afd3e
Tommy-Spurlock <tspurloc@gmail.com>
2023-02-15 11:08:00 -0500

    first commit for fundamental walkthrough

---
+++ main.js
@@ -0,0 +1,101 @@
+
+
+
+//   Variables 
+
+// It's a storage space (container ) for information for us to us later in our code.
+
+// var myName = "Tommy"
+
+const myName = "Tommy"
+
+
+
+let age = 34
+    // console.log(age)
+
+    age = 35
+    // console.log(age)
+
+
+    // Data Types    
+
+    // Integers
+        // It's a number
+
+        let number = 1
+            // console.log("Initial", number)
+
+            number = number + 9
+
+            // console.log("first reassign", number)
+
+            number = number + 10
+            
+            // console.log("second reassign", number)
+
+            // Boolean
+
+            // true or false
+
+
+            const isItRaining = false
+
+            let doILikePickles = true
+
+
+            // string 
+
+            // something wrapped in quotes usually words
+
+            const thisCohort = 'Cohort 7'
+
+            // console.log(thisCohort)
+            // Concatenation
+
+            const introduction = "My name is " + myName
+
+            // console.log(introduction)
+
+            // String Interpolation
+
+            const introV2 = `My name is ${myName}`
+
+            // console.log(introV2)
+            
+
+            // Arrays
+
+            // A List or Collection of information
+
+
+            const array = [1, 2, 3, 4, "Hello", "Mike", "Oscar", true, ["because", "we", "can"]]
+
+            const howManyInArray = array.length
+
+            // console.log(howManyInArray)
+
+            let subArray = array[8]
+
+            // console.log(subArray[1])
+
+
+
+            // Objects
+
+            // store specific information about a thing
+
+
+            const theRock = {
+                name: "Dwayne Johnson",
+                age: 50,
+                canWeSmell: true,
+                movies:["Fast Some Number", "San Andreas", "Jumanji", "Scorpion King", "Jungle Cruise"]
+            }
+
+            console.log(theRock.name)
+            console.log(theRock["name"])
+
+
+            
+
